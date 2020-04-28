3db824e8f58ce65d906ef038bc6c64dd7fd24c97
Eduardo Paredes <eduardo.paredes@rokubun.cat>
2020-04-28 11:34:29 +0200

    feat: add Process Result methods

--- sdk/src/main/java/cat/rokubun/jason/ProcessResult.kt
+++ sdk/src/main/java/cat/rokubun/jason/ProcessResult.kt
@@ -10,44 +10,89 @@ import cat.rokubun.jason.repository.remote.dto.ResultsResponse
 class ProcessResult(val listResultsResponse: List<ResultsResponse>){
 
     /**
-     * return spp KMl url
+     * Returns spp KMl url.
      */
     fun getSppKmlUrl(): String {
-        val sppKmlUrl = listResultsResponse.filter{ it.name.contains("_position_spp.kml")}
-        return sppKmlUrl.last().url
+        return listResultsResponse.find{ it.name.toLowerCase()
+            .contains("spp.kml")}?.url ?: "Url not found"
     }
 
     /**
-     * return spp CSV URL
+     * Returns spp CSV url.
      */
     fun getSppCsvUrl(): String{
-        val sppCsvUrl = listResultsResponse.singleOrNull { it.name.contains("_position_spp.csv") }
-        return sppCsvUrl!!.url
-    }
-
-
-    //TODO
-    fun getPreciseCsvUrl() : Double{
-        return 0.0
-    }
-    //TODO
-    fun getpreciseKmlUrl(): Double{
-        return 0.0
-    }
-    //TODO
-    fun isStatic(): Boolean{
-        return false
-    }
-    //TODO
-    fun  getStaticPosition() {
+        return  listResultsResponse.find { it.name.toLowerCase()
+            .contains("spp.csv") }?.url ?: "Url not found"
 
     }
-    //TODO
-    fun getProcessingType() {
+
+    /**
+     * Returns number of satellites url.
+     */
+    fun getNumSatellitesUrlPng():String{
+        return listResultsResponse.find{ it.name.toLowerCase()
+            .contains( "num_satellites.png")}?.url ?: "Url not found"
 
     }
+
+    /**
+     * Returns precise CSV url.
+     */
+
+    fun getPreciseCsvUrl() : String{
+        return listResultsResponse.find{ it.name.toLowerCase()
+            .contains( "ppp.csv") }?.url ?: "Url not found"
+
+    }
+
+    /**
+     * Returns precise KML url
+     */
+    fun getpreciseKmlUrl(): String{
+        return listResultsResponse.find{ it.name.toLowerCase()
+            .contains( "ppp.kml") }?.url ?: "Url not found"
+    }
+
+    /**
+     * Returns if a process is static
+     */
+    fun isStatic(): Boolean {
+
+        return listResultsResponse.any { !it.staticPos.isNullOrEmpty() }
+    }
+    /**
+     * Returns static position
+     */
+    fun  getStaticPosition(): String {
+
+        return listResultsResponse.lastOrNull()?.staticPos ?: "Static position not found"
+    }
+
+    /**
+     * Returns processing type (SPP, PPP, RTK, PPK)
+     */
+    fun getProcessingType(): String {
+        return  listResultsResponse.findLast { it.name.toLowerCase().contains(".kml") }!!
+            .name.split('_').last().split('.')[0].toUpperCase()
+    }
+
+    /**
+     * Returns sky plot url
+     */
+    fun getSkyPlotUrl(): String{
+        return listResultsResponse.find{ it.name.toLowerCase()
+            .contains( "skyplot.png") }?.url ?: "Url not found"
+    }
+
+    /**
+     * Returns zip url
+     */
+    fun getZipUrl(): String{
+        return listResultsResponse.find{ it.name.toLowerCase()
+            .contains( ".zip") }?.url ?: "Url not found"
+    }
     //TODO
-    fun getNumEpochs() {
+    private fun getNumEpochs() {
 
     }
 
